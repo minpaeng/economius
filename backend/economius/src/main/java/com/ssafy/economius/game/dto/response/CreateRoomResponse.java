f90b5eb951b263536da5ec7f59ad0f67f6873990
qkdk <akb033134@gmail.com>
2023-09-18 14:38:22 +0900

    Feat: 게임방 생성 컨트롤러 구현

---
+++ backend/economius/src/main/java/com/ssafy/economius/game/dto/response/CreateRoomResponse.java
@@ -0,0 +1,12 @@
+package com.ssafy.economius.game.dto.response;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+
+@Data
+@AllArgsConstructor
+public class CreateRoomResponse {
+
+    private int roomId;
+}
