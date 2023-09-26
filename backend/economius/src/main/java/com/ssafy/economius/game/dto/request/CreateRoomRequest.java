cff1d5f08d97e5b8bf6616419acd97edc2dd86b3
권민정 <22alswjd@naver.com>
2023-09-26 22:11:31 +0900

    Fix: 룸 생성 에러 수정중

--- backend/economius/src/main/java/com/ssafy/economius/game/dto/request/CreateRoomRequest.java
+++ backend/economius/src/main/java/com/ssafy/economius/game/dto/request/CreateRoomRequest.java
@@ -1,7 +1,9 @@
 package com.ssafy.economius.game.dto.request;
 
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
+@NoArgsConstructor
 @Data
 public class CreateRoomRequest {
     private Long player;
