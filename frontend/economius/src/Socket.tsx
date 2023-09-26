9a67b0e527c3d8dac2fd1da12ce0415aefefdf92
qkdk <akb033134@gmail.com>
2023-09-27 01:52:57 +0900

    Fix: 머지충돌 오류 처리

--- frontend/economius/src/Socket.tsx
+++ frontend/economius/src/Socket.tsx
@@ -93,7 +93,7 @@ function PlayerSocket() {
 
     useEffect(() => {
         // 소켓 연결
-        stompClient.current = Stomp.over(() => new sockjs('https://j9b109.p.ssafy.io/ws'));
+        stompClient.current = Stomp.over(() => new sockjs('http://j9b109.p.ssafy.io:8080/ws'));
 
         const connectHandler = () => {
             // stompClient가 null인 경우 연결하지 않음
@@ -221,7 +221,6 @@ function PlayerSocket() {
                                 insurance3: message.insuranceDto[3],
                                 insurance4: message.insuranceDto[4],
                             });
->>>>>>> feature/fe/insurance
                         }
                     });
                 }
