6a342a4afa1e3ed2947b6c287c9443345e85cf3c
권민정 <22alswjd@naver.com>
2023-09-27 01:20:31 +0900

    Feat: 초기 데이터 임시 요청

--- backend/economius/src/main/java/com/ssafy/economius/game/controller/GameRoomController.java
+++ backend/economius/src/main/java/com/ssafy/economius/game/controller/GameRoomController.java
@@ -13,6 +13,7 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -38,7 +39,7 @@ public class GameRoomController {
     }
 
     @GetMapping("/{roomId}/start")
-    public ResponseEntity<FinishTurnResponse> start(int roomId) {
+    public ResponseEntity<FinishTurnResponse> start(@PathVariable int roomId) {
         return new ResponseEntity<>(gameService.start(roomId), HttpStatus.OK);
     }
 }
