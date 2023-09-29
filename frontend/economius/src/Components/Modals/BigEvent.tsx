d809bc7407cba56803ebc79a43fd6406e5bfb9d0
qkdk <akb033134@gmail.com>
2023-09-29 23:35:15 +0900

    Feat: 예언소 예언 없는 경우 구현

--- frontend/economius/src/Components/Modals/BigEvent.tsx
+++ frontend/economius/src/Components/Modals/BigEvent.tsx
@@ -1,9 +1,11 @@
 import Modal from 'react-modal';
 import * as S from './BigEvent.style';
-import { useRecoilState } from 'recoil';
-import { IsModalOpenState } from '/src/recoil/animation/atom';
+import * as SS from './GlobalModal.stye';
 
-import { useEffect, useState } from 'react';
+import {useRecoilState} from 'recoil';
+import {IsModalOpenState} from '/src/recoil/animation/atom';
+
+import {useEffect, useState} from 'react';
 
 function BigEvent({issue}) {
     // 원래는 초기값 false로 두고 해당 턴이 되면 true로 바꿔줘야할듯
@@ -15,10 +17,9 @@ function BigEvent({issue}) {
     };
 
     useEffect(() => {
-        if (issue.type === "BOOM"){
+        if (issue.type === "BOOM") {
             setWordColor("black");
-        }
-        else{
+        } else {
             setWordColor("white");
         }
     })
@@ -51,15 +52,41 @@ function BigEvent({issue}) {
         },
     };
 
+    const noneModalStyle: any = {
+        overlay: {
+            position: 'fixed',
+            top: 0,
+            left: 0,
+            right: 0,
+            bottom: 0,
+            backgroundColor: 'rgba(0,0,0,0.6)',
+            zIndex: 10,
+        },
+        content: {
+            display: 'flex',
+            flexDirextion: 'column',
+            backgroundColor: 'rgba(255,255,255,0.95)',
+            overflow: 'auto',
+            zIndex: 10,
+            margin: 'auto',
+            width: '500px',
+            height: '350px',
+            border: '5px solid white',
+            borderRadius: '20px',
+            padding: '0px',
+        },
+    };
+
+
     function getGoldAndInterestRatesChanges() {
         return <S.BigEventRightChange1>{issue.interestRateChange.assetType} {issue.interestRateChange.changePercentage}%
-            /  {issue.goldChange.assetType} {issue.goldChange.changePercentage} %</S.BigEventRightChange1>;
+            / {issue.goldChange.assetType} {issue.goldChange.changePercentage} %</S.BigEventRightChange1>;
     }
 
     function getStockChanges(index) {
         let description = "";
 
-        for (let i = index * 3; i < index * 3 + 3; i++){
+        for (let i = index * 3; i < index * 3 + 3; i++) {
             description += `${issue.stockChanges[i].stockType} ${issue.stockChanges[i].changePercentage} %  / `
         }
 
@@ -67,7 +94,17 @@ function BigEvent({issue}) {
         return <S.BigEventRightChange1>{description}</S.BigEventRightChange1>
     }
 
-    return (
+    return issue.issueId < 0 ?
+        <Modal isOpen={isModalOpen} style={noneModalStyle} onRequestClose={closeModal}>
+            <SS.Main>
+                <SS.Top>
+                    <SS.TopTitle>예언소</SS.TopTitle>
+                </SS.Top>
+                <SS.Mid>
+                    <SS.MidDesc>다음 이벤트가 없습니다.</SS.MidDesc>
+                </SS.Mid>
+            </SS.Main>
+        </Modal> : (
         <Modal isOpen={isModalOpen} style={modalStyle} onRequestClose={closeModal}>
             <S.BigEvent>
                 <S.BigEventLeft/>
