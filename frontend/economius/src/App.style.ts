ca5bc904c6b8beb2747e946db84b0a5f47346b05
Dayoung <everyyoung99@gmail.com>
2023-10-05 15:33:55 +0900

    Fix: 백그라운드 이미지

--- frontend/economius/src/App.style.ts
+++ frontend/economius/src/App.style.ts
@@ -3,7 +3,7 @@ import { styled } from 'styled-components';
 export const Rooter = styled.div`
     width: 100%;
     height: 100%;
-    background-image: url('background/9.png');
+    background-image: url('/background/9.png');
     background-size: 180%;
     background-position: 68% 32%;
 `;
