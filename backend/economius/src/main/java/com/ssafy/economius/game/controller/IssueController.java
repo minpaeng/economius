5dac942ca3efd6fb9fef2426e76ab1c1dbf0be10
권민정 <22alswjd@naver.com>
2023-10-05 20:54:45 +0900

    Feat: 시연용 이슈 셋팅

--- backend/economius/src/main/java/com/ssafy/economius/game/controller/IssueController.java
+++ backend/economius/src/main/java/com/ssafy/economius/game/controller/IssueController.java
@@ -2,7 +2,7 @@ package com.ssafy.economius.game.controller;
 
 import com.ssafy.economius.game.dto.request.OracleRequest;
 import com.ssafy.economius.game.dto.response.IssueResponse;
-import com.ssafy.economius.game.service.OracleService;
+import com.ssafy.economius.game.service.IssueService;
 import java.util.Map;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -16,14 +16,14 @@ import org.springframework.stereotype.Controller;
 @RequiredArgsConstructor
 public class IssueController {
 
-    private final OracleService oracleService;
+    private final IssueService issueService;
     private final SimpMessagingTemplate template;
 
     @MessageMapping(value = "/{roomId}/oracle")
     public void oracle(@DestinationVariable int roomId, OracleRequest oracleRequest) {
         log.info(roomId + ": oracle 호출 -> " + oracleRequest.toString());
 
-        IssueResponse issueResponse = oracleService.oracle(roomId);
+        IssueResponse issueResponse = issueService.issue(roomId);
 
         log.info(roomId + ": oracle 결과 -> " + issueResponse.toString());
         Map<String, Object> headers = Map.of("success", true, "type", "oracle");
@@ -32,7 +32,7 @@ public class IssueController {
     }
 
     public void issue(int roomId) {
-        IssueResponse issueResponse = oracleService.oracle(roomId);
+        IssueResponse issueResponse = issueService.issue(roomId);
 
         log.info(roomId + ": issue 결과 -> " + issueResponse.toString());
         Map<String, Object> headers = Map.of("success", true, "type", "issue");
