d44954dfb54e493b78b20e647a90beb28ddc266b
uttamapaksa <uttamapaksa@gmail.com>
2023-10-05 22:55:11 +0900

    Feat: stepsound

--- frontend/economius/src/Audio.tsx
+++ frontend/economius/src/Audio.tsx
@@ -1,2 +1,3 @@
 export const effectAudioPopup = new Audio('/effectSound/modal-popup.mp3');
 export const effectAudioClick = new Audio('/effectSound/click.wav');
+export const effectAudioStep = new Audio('/effectSound/foot-steps.wav');
