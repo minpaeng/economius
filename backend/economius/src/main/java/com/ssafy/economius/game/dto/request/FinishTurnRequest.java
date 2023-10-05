e8fdcf6bec53285a511643c750e3943512a90888
권민정 <22alswjd@naver.com>
2023-10-05 21:19:00 +0900

    Fix: finishTurn 오류 수정

--- backend/economius/src/main/java/com/ssafy/economius/game/dto/request/FinishTurnRequest.java
+++ backend/economius/src/main/java/com/ssafy/economius/game/dto/request/FinishTurnRequest.java
@@ -1,8 +1,10 @@
 package com.ssafy.economius.game.dto.request;
 
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.ToString;
 
+@NoArgsConstructor
 @Getter
 @ToString
 public class FinishTurnRequest {
